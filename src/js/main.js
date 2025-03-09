9b4d458b5f75fc39eb26432e46ef3b18ba7bd4f5
VickyJourney <viktoria.diordiczuk@gmail.com>
2025-03-09 11:05:25 +0100

    add hlegal homepage

---
+++ src/js/main.js
@@ -0,0 +1,61 @@
+import '../scss/style.scss'
+
+(function () {
+    const isOpen = document.querySelector(".header__burger");
+    const isClose = document.querySelector(".header__menu-close");
+    const navigation = document.querySelector(".header__menu");
+
+    if (isOpen && navigation) {
+        isOpen.addEventListener("click", () => {
+            navigation.classList.toggle("active");
+        });
+    }
+
+    if (isClose && navigation) {
+        isClose.addEventListener("click", () => {
+            navigation.classList.remove("active");
+        });
+    }
+
+    document.addEventListener("click", (event) => {
+        if (navigation && !navigation.contains(event.target) && isOpen && !isOpen.contains(event.target)) {
+            navigation.classList.remove("active");
+        }
+    });
+})();
+
+
+(function () {
+  const tabs = document.querySelectorAll(".tabs-item");
+  const contents = document.querySelectorAll(".tabs__content-text");
+    tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabs.forEach(n => n.classList.remove("active"));
+      contents.forEach(m => m.classList.remove("active"));
+      tab.classList.add("active");
+      contents[index].classList.add("active");
+    });
+  });
+})();
+
+
+(function () {
+  const tabs = document.querySelectorAll(".header__lang-link");
+    tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabs.forEach(n => n.classList.remove("active"));
+      tab.classList.add("active");
+    });
+  });
+})();
+
+(function () {
+  const tabs = document.querySelectorAll(".header__menu-item");
+    tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabs.forEach(n => n.classList.remove("active"));
+      tab.classList.add("active");
+    });
+  });
+})();
+
